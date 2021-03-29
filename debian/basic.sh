264505331f9dd7c6d1718e2c21ddaad793d8e686
David Ding <chengjie.ding@gmail.com>
2021-03-29 15:08:37 +0800

    Add basic application. (#2)

---
+++ debian/basic.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt update
+sudo apt install curl -y
+
+sudo apt install net-tools -y
+# ping
+sudo apt install iputils-ping -y
+
+# Build tools
+sudo apt install build-essential -y
+sudo apt install cmake -y
+
+# CMD line tools
+sudo apt install tree -y
+sudo apt install mc -y
+sudo apt install sudo -y
+sudo apt install nload -y
+sudo apt install tmux -y
+sudo apt install zsh -y
