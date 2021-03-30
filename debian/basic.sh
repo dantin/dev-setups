945440a25b3f942184af00b191bb97e07d2816de
David Ding <chengjie.ding@gmail.com>
2021-03-30 14:20:18 +0800

    support customized Vim. (#3)
    * update git ignore file.
    
    * add vim compile scripts.

--- debian/basic.sh
+++ debian/basic.sh
@@ -11,6 +11,7 @@ sudo apt install iputils-ping -y
 # Build tools
 sudo apt install build-essential -y
 sudo apt install cmake -y
+sudo apt install cscope -y
 
 # CMD line tools
 sudo apt install tree -y
---
+++ debian/pre_install_pyenv.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt update
+# Source dependencies
+sudo apt-get install --no-install-recommends \
+	make \
+	build-essential \
+	libssl-dev \
+	zlib1g-dev \
+	libbz2-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	wget \
+	curl \
+	llvm \
+	libncurses5-dev \
+	xz-utils \
+	tk-dev \
+	libxml2-dev \
+	libxmlsec1-dev \
+	libffi-dev \
+	liblzma-dev \
+	-y
